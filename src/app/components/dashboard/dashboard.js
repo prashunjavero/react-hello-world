ec86c34b25b9288516ce98d306d45fee1c9254fb
prashunjavero <javeriprashun2@gmail.com>
2021-04-11 15:38:41 +0200

    fixing the chart load

--- src/app/components/dashboard/dashboard.js
+++ src/app/components/dashboard/dashboard.js
@@ -1,9 +1,129 @@
-import React, { Component }  from 'react';
+import React, { Component, useEffect, useRef, useState }  from 'react';
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+} from 'chart.js';
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+);
+
+
+
+const labels = Object.keys(localStorage);
+const dataArray = [] ;
+const labelArray = [];
+
+let getData = () => {
+
+const data = {
+  labels: Object.keys(localStorage),
+  datasets: [{
+    label: 'Votes by Restaurant',
+    data: Object.values(localStorage),
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  }]
+};
+return data;
+}
+
+let getonfig = (data) => {
+const chartConfig = {
+  type: 'bar',
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  },
+};
+return chartConfig;
+}
 
 function Dashboard() {
+  const chartContainer = React.useRef();
+  const [chartInstance, setChartInstance] = React.useState();
+  const [, updateComponent] = React.useState();
+  const forceUpdateComponent = React.useCallback(() => updateComponent({}), []);  
+   
+
+  useEffect(() => {
+    if (chartContainer && chartContainer.current) {
+      let data = getData();
+      console.log(data)
+      const newChartInstance = new Chart(chartContainer.current, getonfig(data));
+      setChartInstance(newChartInstance);
+    }
+   
+  }, [chartContainer]);
+
     return (
       <div className="Dashboard">
-          <p>Dashboard</p>
+        <canvas ref={chartContainer} />
       </div>
     );
   }
--- src/app/components/voting/voting.js
+++ src/app/components/voting/voting.js
@@ -97,7 +97,7 @@ function Voting (){
                       }}>Reset</Button>
                     </Box>
                }
-      </div>
+           </div>
     );
   }
   
